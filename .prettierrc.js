217b1e7605c3009a125ee7be390a78b8b459d8be
Giga Amiridze <giga.amiridze@azry.com>
2024-05-06 23:28:42 +0400

    Modified App.tsx file

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
+  trailingComma: 'all',
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
-  trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
 };
