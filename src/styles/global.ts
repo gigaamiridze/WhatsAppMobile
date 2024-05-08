41da821c6fca6f8497e46b4fe9a8fc2c441d9688
Giga Amiridze <giga.amiridze@azry.com>
2024-05-08 20:38:58 +0400

    Added global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+import { horizontalScale } from '../utils';
+import { Colors, Sizes } from '../constants';
+
+const globalStyles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+  backgroundWhite: {
+    backgroundColor: Colors.WHITE,
+  },
+  backgroundPrimary: {
+    backgroundColor: Colors.PRIMARY,
+  },
+  paddingHorizontal: {
+    paddingHorizontal: horizontalScale(Sizes.SMALL),
+  },
+  screenContainer: {
+    flex: 1,
+    backgroundColor: Colors.PRIMARY,
+    paddingHorizontal: horizontalScale(Sizes.SMALL),
+  },
+});
+
+export default globalStyles;
